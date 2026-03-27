ca986fcb024223f23b63568fbf3e050dd0d69dea
chamoionak <2591558509@qq.com>
2026-03-28 01:12:43 +0800

    添加缓存，更新缓存策略，解决缓存穿透问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -10,8 +10,11 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -31,20 +34,42 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         String key=CACHE_SHOP_KEY+id;
         //1.先在Redis中查询
         String shopJson = stringRedisTemplate.opsForValue().get(key);
-        //2.若命中直接返回
+        //2.若命中直接返回(只有有数据的情况下才会触发如json=“ABC”)
         if(StrUtil.isNotBlank(shopJson)){
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
+        }
+        //不等于null相当于等于“”，所以这里用的是!=null来判断是否命中
+        if(shopJson!=null){
+            return Result.fail("店铺不存在");
         }
         //3.若未命中，根据ID查询数据库
         Shop shop = getById(id);
         if(shop==null)
         {
+            //将null写入redis
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
             //5.ID不一致则返回404
             return Result.fail("店铺不存在");
         }
         //4.ID一致则返回，将查询结果放入Redis
-        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop));
+        stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return Result.ok(shop);
+    }
+
+    @Override
+    @Transactional
+    public Result update(Shop shop) {
+        Long id=shop.getId();
+        if(id==null)
+        {
+            return Result.fail("店铺ID不能为空");
+        }
+        //更新数据库
+        updateById(shop);
+        //删除缓存
+        String key=CACHE_SHOP_KEY+id;
+        stringRedisTemplate.delete(key);
+        return  Result.ok();
     }
 }
